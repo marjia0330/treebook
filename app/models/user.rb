033090ec66bcbd6236066732d3a2c66ab5be13bb
Marjia Yeasmin <marjia0330@gmail.com>
2013-06-21 23:24:18 +1000

    Added gravatar

--- app/models/user.rb
+++ app/models/user.rb
@@ -26,4 +26,11 @@ class User < ActiveRecord::Base
   	first_name + " " + last_name
   end
 
+  def gravatar_url
+    stripped_email = email.stripped_email
+    downcased_email = stripped_email.downcase
+    hash = Digest::MD5.hexdigest(downcased_email)
+    "http://gravatar.com/avatar/#{hash}"
+  end
+
 end
